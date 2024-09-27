2fc64b351bfa8da324c5b1a8dc529f6b3ff62c53
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2024-09-28 00:55:34 +0800

    Add files via upload

---
+++ MInecraft Injection Client/JNI.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+
+#include <memory>
+#include <Windows.h>
+#include <jni.h>
+
+class JNI final
+{
+public:
+    JNI()
+    {
+        JavaVM* p_jvm{ nullptr };
+        jsize vm_count{ 0 };
+        jint result = JNI_GetCreatedJavaVMs(&p_jvm, 1, &vm_count);
+        if (result != JNI_OK || vm_count == 0)
+        {
+            printf("[-] JNI() failed to initialize p_jvm\n");
+            MessageBoxA(0, "ERROR", "Check console", MB_ICONASTERISK);
+            return;
+        }
+
+        JavaVMAttachArgs attach_args;
+        attach_args.version = JNI_VERSION_10;
+        attach_args.name = nullptr;
+        attach_args.group = nullptr;
+        if (p_jvm->AttachCurrentThread(reinterpret_cast<void**>(&p_env), &attach_args) != JNI_OK)
+        {
+            printf("[-] Failed to attach current thread to JVM\n");
+            MessageBoxA(0, "ERROR", "Check console", MB_ICONASTERISK);
+            return;
+        }
+
+        this->p_jvm = p_jvm;
+        is_init = true;
+    }
+
+    ~JNI()
+    {
+        if (is_init)
+        {
+            p_jvm->DetachCurrentThread();
+            is_init = false;
+        }
+    }
+
+    JNIEnv* GetEnv()
+    {
+        if (!is_init)
+        {
+            printf("[-] JNI environment not initialized.\n");
+            return nullptr;
+        }
+        return p_env;
+    }
+
+    void DetachCurrentThread()
+    {
+        if (is_init && p_jvm)
+        {
+            p_jvm->DetachCurrentThread();
+        }
+    }
+
+private:
+    JavaVM* p_jvm{ nullptr };
+    JNIEnv* p_env{ nullptr };
+    bool is_init{ false };
+};
+
+inline std::unique_ptr<JNI> p_jni;
---
+++ MInecraft Injection Client/main.cpp
@@ -0,0 +1,136 @@
+﻿#include <windows.h>
+#include <thread>
+#include <cstdio>
+#include <jni.h>
+#include <chrono>
+#include <memory>
+
+#include "JNI.h"
+#include "Hooks.h"
+
+std::atomic<bool> g_running{ true }; 
+
+const char* MINECRAFT_OBFUSCATED_CLASS_NAME = "ave";
+
+void MainThread(HMODULE instance)
+{
+    if (!p_jni) {
+        printf("JNI initialization failed\n");
+        return;
+    }
+    else
+    {
+        printf("JNI initialization succeeded\n");
+    }
+
+    JNIEnv* p_env = p_jni->GetEnv();
+    if (!p_env) {
+        printf("Failed to get JNI environment\n");
+        return;
+    }
+    else
+    {
+        printf("Get JNI env succeeded\n");
+    }
+
+    while (g_running && !(GetAsyncKeyState(VK_END) & 0x8000)) {
+        Sleep(100);  
+    }
+
+    p_jni->DetachCurrentThread();
+    FreeLibraryAndExitThread(instance, 0);
+}
+
+bool __stdcall DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
+{
+    static FILE* p_file{ nullptr };
+    static std::thread main_thread;
+
+    if (reason == DLL_PROCESS_ATTACH) {
+        AllocConsole();
+        freopen_s(&p_file, "CONOUT$", "w", stdout);
+        printf("DLL Loaded\n");
+        
+        p_jni = std::make_unique<JNI>();
+        p_hooks = std::make_unique<Hooks>();
+
+        main_thread = std::thread([instance] { MainThread(instance); });
+        if (main_thread.joinable())
+            main_thread.detach();
+    }
+    else if (reason == DLL_PROCESS_DETACH) {
+        printf("DLL Unloading\n");
+
+        g_running = false;
+
+        if (main_thread.joinable()) {
+            main_thread.join();
+        }
+
+        p_jni.reset();
+        p_hooks.reset();
+
+        if (p_file) {
+            fclose(p_file);
+            p_file = nullptr;
+        }
+        FreeConsole();
+    }
+
+    return true;
+}
+
+JavaVM* g_jvm = nullptr;
+
+JNIEnv* GetJNIEnv() {
+    if (!g_jvm) return nullptr;
+
+    JNIEnv* env = nullptr;
+    g_jvm->AttachCurrentThread(reinterpret_cast<void**>(&env), nullptr);
+    return env;
+}
+
+jclass GetMinecraftClass() {
+    JNIEnv* env = GetJNIEnv();
+    if (!env) {
+        printf("Failed to get JNI environment\n");
+        return nullptr;
+    }
+
+    jclass minecraftClass = env->FindClass(MINECRAFT_OBFUSCATED_CLASS_NAME);
+    if (!minecraftClass) {
+        printf("Failed to find Minecraft class: %s \n" , MINECRAFT_OBFUSCATED_CLASS_NAME);
+        return nullptr;
+    }
+
+    return minecraftClass;
+}
+
+jobject GetMinecraftInstance() {
+    JNIEnv* env = GetJNIEnv();
+    if (!env) {
+        printf("Failed toget JNI environment \n");
+        return nullptr;
+    }
+
+    jclass minecraftClass = GetMinecraftClass();
+    if (!minecraftClass) {
+        printf("Minecraft class not found \n");
+        return nullptr;
+    }
+
+    jmethodID getInstanceMethod = env->GetStaticMethodID(minecraftClass, "getInstance", "()Lave;");
+    if (!getInstanceMethod) {
+        printf("Failed to find getInstance method \n");
+        return nullptr;
+    }
+
+    jobject minecraftInstance = env->CallStaticObjectMethod(minecraftClass, getInstanceMethod);
+    if (!minecraftInstance) {
+        printf("Failed to get Minecraft instance \n");
+        return nullptr;
+    }
+
+    return minecraftInstance;
+}
+
